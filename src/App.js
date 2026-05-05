519ddd1d6c2ad7c014be415d9362d951576dacba
Yngridmartiniano <p.yngrid@escola.pr.gov.br>
2026-05-05 12:29:20 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Header from "./componentes/Header";
+import Footer from "./componentes/Footer";
+import Main from "./componentes/Main";
 
 function App() {
   return (
     <>
+    <Header />
+    <Main />
+    <Footer />
     </>
   );
 }
---
+++ src/componentes/Header.js
@@ -0,0 +1,21 @@
+function Header(){
+
+    return (
+       <>
+        <div className="headerTopo">
+       <h1>Curso Técnico em Desenvolvimento de sistemas</h1>
+      </div>
+      <nav className="headerMenu">
+        <ul>
+            <li>Home</li>
+            <li>Curso</li>
+            <li>Diciplinas Básicas</li>
+            <li>Diciplinas Técnicas</li>
+            <li>Equipe</li>
+        </ul>
+      </nav>
+       </>
+    )
+  }
+
+  export default Header
